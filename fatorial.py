4cd4cd29317984a281aac8212889da7659f3b72e
kiritogtsa <tenseishitarrakenseshita@gmail.com>
2023-11-30 21:14:18 -0300

    fatorial melhorado

--- fatorial.py
+++ fatorial.py
@@ -1,7 +1,15 @@
-def inicio():
-    numero=int(input("digite o numero que vc quer saver o fatorial"))
-    soma=1
-    for i in range(1,numero+1):
-        soma*=i
-    print(soma)
-inicio()
+# jeito recursivo 
+# def calculo(x):
+#     if x==0 or x==1:
+#         return 1
+#     else:
+#         return x * calculo(x-1)
+# print(calculo(int(input("digite um numero na moral ai: "))))
+# jeito normal de fazer isso
+# def inicio():
+#     x=int(input("digite um numero na moral ai:"))
+#     fatorial=1
+#     for i in range(1,x+1):
+#         fatorial*=i
+#     print(fatorial)
+# inicio()
